e14113250b8040e749e84b3ab5078e045a951853
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-10-12 23:19:23 +0530

    updated android manifest file and added button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { selectContactPhone } from 'react-native-select-contact';
 
+function getPhoneNumber() {
+  return selectContactPhone().then((selection) => {
+    if (!selection) {
+      return null;
+    }
+
+    const { contact, selectedPhone } = selection;
+    console.log(
+      `Selected ${selectedPhone.type} phone number ${selectedPhone.number} from ${contact.name}`
+    );
+    return selectedPhone.number;
+  });
+}
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Import contacts" onPress={() => getPhoneNumber()} />
     </View>
   );
 }
