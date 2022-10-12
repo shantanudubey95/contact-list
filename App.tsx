91ee2c02b51b2c184150905942db8711aa349f2a
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-10-12 23:59:25 +0530

    showing contact on ui

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { selectContactPhone } from 'react-native-select-contact';
 
-function getPhoneNumber() {
-  return selectContactPhone().then((selection) => {
-    if (!selection) {
-      return null;
-    }
-
-    const { contact, selectedPhone } = selection;
-    console.log(
-      `Selected ${selectedPhone.type} phone number ${selectedPhone.number} from ${contact.name}`
-    );
-    return selectedPhone.number;
-  });
-}
 export default function App() {
+  const [selectcontact, setContact] = React.useState('');
+
+  function getPhoneNumber() {
+    return selectContactPhone().then((selection) => {
+      if (!selection) {
+        return null;
+      }
+
+      const { contact, selectedPhone } = selection;
+      console.log(
+        `Selected ${selectedPhone.type} phone number ${selectedPhone.number} from ${contact.name}`
+      );
+      setContact(selectedPhone.number);
+      return selectedPhone.number;
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Selected Contact is {selectcontact}</Text>
       <Button title="Import contacts" onPress={() => getPhoneNumber()} />
     </View>
   );
